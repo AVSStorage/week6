45b5b86a9919645e6776288bf1d3d505ef545393
Alice <alise-fox@inbox.ru>
2020-12-27 22:20:12 +0300

    Week 8 - again

--- app.js
+++ app.js
@@ -15,20 +15,48 @@ app.get('/login/', (req, res) => {
 })
 
 
-
-app.get('/test/', async (req,res) => {
-    const URL = req.query.URL
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox',  '--disable-setuid-sandbox']})
-    const page = await browser.newPage();
-    await page.goto(URL)
-    await page.waitForSelector('#bt')
-    await page.click('#bt')
-    const result = await page.$eval('#inp', el => el.value)
-    browser.close()
-    res.send(result)
+app.get('/wordpress/wp-json/wp/v2/', (req,res) => {
+    
+     res.json({
+        id: 1,
+        title: 'alisefox'
+    })
 })
 
+app.post('/render/', (req, res) => {
+  const {random2, random3} = req.body
 
+  http.get( req.query.addr, (resFrom) => {
+    const { statusCode } = resFrom;
+    let error;
+
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+      `Status Code: ${statusCode}`);
+      resFrom.resume();
+      return;
+    } 
+
+
+    resFrom.setEncoding('utf8');
+    let rawData = '';
+    resFrom.on('data', (chunk) => { rawData += chunk; });
+    resFrom.on('end', () => {
+      try {
+        writeFileSync('views/template.pug', rawData, function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        }); 
+        res.render('template.pug', {random2, random3})
+      } catch (e) {
+        res.status(500)
+      }
+    });
+      }).on('error', (e) => {
+      res.status(500)
+      }).end();
+      
+ })     
 
 app.all('*', function( req, res) {
     res.send('alisefox')
